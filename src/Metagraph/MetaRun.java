b3626497401da0166d797af3ab6ddd2f9d7f1736
Prudhvi Boyapalli <prrb02@gmail.com>
2016-01-02 10:13:20 +0530

    Added ability to run from command line args

--- src/Metagraph/MetaRun.java
+++ src/Metagraph/MetaRun.java
@@ -15,21 +15,27 @@ import java.io.IOException;
  * Loads a graph from file and applies the algorithm
  */
 public class MetaRun {
-//    public static void main(String args[]) throws IOException {
-//        Graph g = new SingleGraph("Custom3");
-//        FileSourceDOT fs = new FileSourceDOT();
-//        fs.addSink(g);
-//        fs.readAll("graphs/Custom3/custom3.dot");
-//
-//        for (Edge e : g.getEdgeSet()) {
-//            e.setAttribute("ui.label", e.getAttribute("capacity").toString());
-//        }
-//
-////        g.display();
-//        run(g, "S", "T", 7, g.getId());
-//    }
+    public static void main(String args[]) throws IOException {
+        System.out.println("Args: " + args[0]);
+        String input = args[0];
+        String start = args[1];
+        String target = args[2];
+        int flow = Integer.parseInt(args[3]);
 
-    public void run(Graph g, String start, String target, int desiredFlow, String graphName, boolean pruning) {
+        Graph g = new SingleGraph(input);
+        FileSourceDOT fs = new FileSourceDOT();
+        fs.addSink(g);
+        fs.readAll("graphs/" + input + "/" + input + ".dot");
+
+        // Label edges with their capacity for later viewing
+        for (Edge e : g.getEdgeSet()) {
+            e.setAttribute("ui.label", e.getAttribute("capacity").toString());
+        }
+
+        run(g, start, target, flow, g.getId(), true);
+    }
+
+    public static void run(Graph g, String start, String target, int desiredFlow, String graphName, boolean pruning) {
         MetaGraphCreation mgs = new MetaGraphCreation();
         mgs.constructMetaGraph(g, start, target, desiredFlow, pruning);
         MetaGraph mg = mgs.getMeta();
@@ -59,7 +65,7 @@ public class MetaRun {
         }
     }
 
-    public void writeGraph(Graph g, String name) throws IOException {
+    public static void writeGraph(Graph g, String name) throws IOException {
         FileSink fs = new FileSinkDOT();
         fs.writeAll(g, "graphs/" + name);
     }
